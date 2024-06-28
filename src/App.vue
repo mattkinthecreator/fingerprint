7c4213289be88fcc7232784e25d654971dd80c0b
Matthew <bekievbeil@gmail.com>
2024-06-28 11:22:50 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+import { onMounted, ref } from 'vue';
+
+const fpPromise = FingerprintJS.load();
+
+const resultRef = ref();
+
+onMounted(async () => {
+  const fp = await fpPromise;
+  const result = await fp.get();
+  resultRef.value = result;
+});
+</script>
+
+<template>
+  <pre>
+    {{ JSON.stringify(resultRef, null, 2) }}
+  </pre>
+</template>
+
+<style scoped></style>
